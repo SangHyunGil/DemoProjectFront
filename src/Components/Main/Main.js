205fdbbb1f3475e664c6791c0651c252c367e481
syoo970 <syu9710@gmail.com>
2021-12-23 23:21:48 +0900

    Design: 메인 페이지 디자인 추가와 메뉴바 일부 변경

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,191 +1,272 @@
-import React, {useEffect} from 'react';
-import styled from 'styled-components';
-import { motion ,useViewportScroll, useTransform, useAnimation } from 'framer-motion';
-import {ReactComponent as Group} from '../../Assets/group-study.svg';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import {
+  motion,
+  useAnimation,
+} from "framer-motion";
+import { ReactComponent as Group } from "../../Assets/group-study.svg";
 import { useInView } from "react-intersection-observer";
 
 const Header = styled(motion.section)`
-    height: 70vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(220, 240, 249,1);
-    @media (max-width: 768px) {
-        flex-direction: column;
-    }
+  height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(220, 240, 249, 1);
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const HeaderTextArea = styled(motion.div)`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  padding-left: 10vw;
+  @media (max-width: 768px) {
+    align-items: center;
+    padding: 0 20px;
+    text-align: center;
+  }
+  @media (max-height: 650px) {
     flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    flex-direction: column;
-    padding-left: 10vw;
-    @media (max-width: 768px) {
-        align-items: center;
-        padding: 0 20px;
-        text-align: center;
-    }
-    @media (max-height: 650px) {
-        flex:1;   
-        padding: 0;
-        align-items: center;
-    }
+    padding: 0;
+    align-items: center;
+  }
 `;
 
 const HeaderMainText = styled(motion.h1)`
-    color: rgba(13, 58, 92,1);
-    @font-face {
-        font-family: 'OTWelcomeBA';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/OTWelcomeBA.woff2') format('woff2');
-        font-weight: normal;
-        font-style: normal;
-    }
-    font-family: 'OTWelcomeBA', sans-serif;
-    font-size: 3rem;
+  color: rgba(13, 58, 92, 1);
+  font-family: "OTWelcomeBA", sans-serif;
+  font-size: 3rem;
 `;
 
 const HeaderSubText = styled(motion.p)`
-    color: rgba(13, 58, 92,1);
-    font-size: 1.5rem;
+  color: rgba(13, 58, 92, 1);
+  font-size: 1.5rem;
 `;
 
 const HeaderImgWrapper = styled(motion.div)`
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    @media (max-height: 650px) {
-        display: none;
-    }
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media (max-height: 650px) {
+    display: none;
+  }
 `;
 
 const HeaderImg = styled(Group)`
+  transform: scale(1);
+  width: 80%;
+  @media (min-width: 400px) and (max-width: 768px) {
+    transform: scale(1.5);
+  }
+  @media (min-width: 399px) {
     transform: scale(1);
-    width: 80%;
-    @media (min-width: 400px) and (max-width: 768px) {
-        transform: scale(1.5);
-    }
-    @media (min-width: 399px) {
-        transform: scale(1);
-    }
+  }
 `;
 
-const Main = styled(motion.section)`
-    
-`;
+const Main = styled(motion.section)``;
 
 const CardWrapper = styled(motion.div)`
-    display: grid;
-    padding: 20px 10vw;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    gap: 5% 3%;
-    @media (max-width: 800px) {
-        row-gap: 10px;
-    }
+  display: grid;
+  padding: 20px 10vw;
+  background: rgba(185, 199, 210, 1);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 5% 3%;
+  @media (max-width: 800px) {
+    row-gap: 10px;
+  }
 `;
 
 const MainCards = styled(motion.div)`
-    padding: 20px;
-    box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
-    background: rgba(210, 218, 226,1.0);
+  padding: 20px;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  background: rgba(210, 218, 226, 1);
 `;
 
 const InfoSection = styled(motion.div)`
-    height: 100vh;
-    background: rgba(210, 218, 226,1.0);
+  height: 100vh;
+  background: rgba(144, 173, 205, 1);
+`;
+
+const InfoSectionHeadTextWrapper = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "OTWelcomeBA", sans-serif;
+  h2 {
+    flex-shrink: 0;
+    font-size: 3rem;
+    padding-top: 2rem;
+  }
+  p {
+    flex-shrink: 0;
+  }
+`;
+
+const InfoSectionHeadImgWrapper = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  margin-top: 10vh;
+  img {
+    height: 40vh;
+  }
 `;
 
 const TextVariants = {
-    start: {
-        opacity: 0,
-        y: 100,
+  start: {
+    opacity: 0,
+    y: 100,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 2,
     },
-    end: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 2,
-        }
-    }
+  },
 };
 
-
 const cardVariants = {
-    hidden: {
-        opacity: 0,
-        y: 100
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8,
     },
-    visible: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            type: "spring",
-            bounce: 0.4,
-            duration: 0.8
-        }
-    }
+  },
 };
 
 const variants = {
-    visible: {
-      y: 0,
-      opacity: 1,
-      transition: {delayChilden: 0.3, staggerChildren: 0.3 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChilden: 0.3,
+      staggerChildren: 0.3,
     },
-    hidden: {
-      y: 100,
-      opacity: 0,
-    },
-    out: {
-        opacity: 0,
-    }
+  },
+  hidden: {
+    opacity: 0,
+  },
 };
 
+const InfoTextVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+      delayChilden: 0.3,
+      staggerChildren: 0.3,
+    },
+  },
+};
 
+const InfoImgVariants = {
+  hidden: {
+    opacity: 0,
+    y:100,
+  },
+  visible: {
+    opacity: 1,
+    y:0,
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
+
+const InfoTextItemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const Temp = () => {
-    const { scrollYProgress } = useViewportScroll();
-    const scale = useTransform(scrollYProgress, [0.3, 1], [0.2, 1]);
-    const animation = useAnimation();
+  const animation = useAnimation();
+  const ImgAnimation = useAnimation();
+  const [ref, inView] = useInView();
+  const [ref2, inView2] = useInView({threshold:0.2});
 
-    return (
-        <React.Fragment>
-            <Header transition={{duration:2,delayChildren:0.5}} initial={{opacity:0}} animate={{opacity:1}}>
-                <HeaderTextArea>
-                    <HeaderMainText variants={TextVariants} initial="start" animate="end" >XX에 오신것을 환영합니다!</HeaderMainText>
-                    <HeaderSubText variants={TextVariants} initial="start" animate="end">
-                        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                    </HeaderSubText>
-                </HeaderTextArea>
-                <HeaderImgWrapper variants={TextVariants} initial="start" animate="end">
-                    <HeaderImg />
-                </HeaderImgWrapper>
-            </Header>
-            <Main>
-               <CardWrapper initial="hidden" animate="visible" variants={variants} exit="out">
-                    <MainCards variants={cardVariants} >
-                        <p>test</p>
-                    </MainCards>
-                    <MainCards variants={cardVariants} >
-                        <p>test</p>
-                    </MainCards>
-                    <MainCards variants={cardVariants} >
-                        <p>test</p>
-                    </MainCards>
-                    <MainCards variants={cardVariants} >
-                        <p>test</p>
-                    </MainCards>
-                </CardWrapper>
-                <InfoSection>
-                    <h2>INFO SECTION!</h2>
-                    <p>Not Designed Yet!</p>
-                </InfoSection>
-            </Main>
-            
-        </React.Fragment>
-    )
-}
+  useEffect(() => {
+    inView? animation.start("visible"): animation.start("hidden");
+    inView2? ImgAnimation.start("visible"): ImgAnimation.start("hidden");
+  }, [animation, inView, ImgAnimation, inView2]);
+
+  return (
+    <React.Fragment>
+      <Header
+        transition={{ duration: 2, delayChildren: 0.5 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        <HeaderTextArea>
+          <HeaderMainText variants={TextVariants} initial="start" animate="end">
+            XX에 오신것을 환영합니다!
+          </HeaderMainText>
+          <HeaderSubText variants={TextVariants} initial="start" animate="end">
+            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit..."
+          </HeaderSubText>
+        </HeaderTextArea>
+        <HeaderImgWrapper variants={TextVariants} initial="start" animate="end">
+          <HeaderImg />
+        </HeaderImgWrapper>
+      </Header>
+      <Main>
+        <CardWrapper initial="hidden" animate="visible" variants={variants}>
+          <MainCards variants={cardVariants}>
+            <p>test</p>
+          </MainCards>
+          <MainCards variants={cardVariants}>
+            <p>test</p>
+          </MainCards>
+          <MainCards variants={cardVariants}>
+            <p>test</p>
+          </MainCards>
+          <MainCards variants={cardVariants}>
+            <p>test</p>
+          </MainCards>
+        </CardWrapper>
+        <InfoSection>
+          <InfoSectionHeadTextWrapper
+            ref={ref}
+            variants={InfoTextVariants}
+            initial="hidden"
+            animate={animation}
+          >
+            <motion.h2 variants={InfoTextItemVariants}>ABOUT US</motion.h2>
+            <motion.p variants={InfoTextItemVariants}>
+              스터디원이 고민이세요?
+            </motion.p>
+          </InfoSectionHeadTextWrapper>
+          <InfoSectionHeadImgWrapper ref={ref2} variants={InfoImgVariants} initial="hidden" animate={ImgAnimation}>
+            <motion.img src="worry.png" alt="worry"></motion.img>
+          </InfoSectionHeadImgWrapper>
+        </InfoSection>
+      </Main>
+      <footer>
+          <p>&copy;copyright, 길상현 유승범</p>
+      </footer>
+    </React.Fragment>
+  );
+};
 
 export default Temp;
--- src/Components/Study/StudyBoard.js
+++ src/Components/Study/StudyBoard.js
@@ -1,56 +1,91 @@
-import React, {useState} from 'react';
-import {Outlet, useParams} from 'react-router-dom';
-import { useSelector} from "react-redux";
-import {getBoardCategory} from '../../Api/Api';
-import {useQuery} from 'react-query';
-import {getCookie} from '../../utils/cookie';
-import {CategoryWrapper,Category,UnderLine} from '../Categories/Categories';
+import React, { useState } from "react";
+import { Outlet, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getBoardCategory } from "../../Api/Api";
+import { useQuery } from "react-query";
+import { getCookie } from "../../utils/cookie";
+import { Category, UnderLine } from "../Categories/Categories";
+import styled from "styled-components";
 
 const UnderLineVariants = {
-    hidden: {
-        opacity: 0,
-    },
-    visible: {
-        opacity: 1,
-    }
-}
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
+
+const CategoryWrapper = styled.div`
+    display: flex;
+    padding: 1rem;
+    border-bottom: 2px solid #e6e6e6;
+`;
 
 function StudyBoard() {
-    const [IsGranted, setIsGranted] = useState(false);
-    const [IsSelected, setIsSelected] = useState('공지사항');
-    const {studyInfos} = useSelector(state => state.users);
-    const {studyId} = useParams();
-    const {data:category} = useQuery(['getBoardCategory',studyId],()=>getBoardCategory(studyId,getCookie('accessToken')),{
-        select: (cat) => cat.data.data,
-        onSuccess: ()=>{
-            const Role  = studyInfos.find(x=>x.studyId===Number(studyId))?.studyRole;
-            console.log(Role);
-            if (Role === 'CREATOR' || Role === 'ADMIN') {
-                setIsGranted(true);
-            }
-            else {
-                setIsGranted(false);
-            }
-        },
-        retry: false,
-    });
+  const [IsGranted, setIsGranted] = useState(false);
+  const [IsSelected, setIsSelected] = useState("공지사항");
+  const { studyInfos } = useSelector((state) => state.users);
+  const { studyId } = useParams();
+  const { data: category } = useQuery(
+    ["getBoardCategory", studyId],
+    () => getBoardCategory(studyId, getCookie("accessToken")),
+    {
+      select: (cat) => cat.data.data,
+      onSuccess: () => {
+        const Role = studyInfos.find(
+          (x) => x.studyId === Number(studyId)
+        )?.studyRole;
+        console.log(Role);
+        if (Role === "CREATOR" || Role === "ADMIN") {
+          setIsGranted(true);
+        } else {
+          setIsGranted(false);
+        }
+      },
+      retry: false,
+    }
+  );
 
-    return (
-        <>
-            <CategoryWrapper>
-                {category?.map((cat) => (
-                <Category 
-                activeclassname="active"
-                onClick={()=>setIsSelected(cat.title)} 
-                to={`/study/${studyId}/board/${cat.studyBoardId}/articles`} 
-                key={cat.studyBoardId}>{cat.title}{IsSelected === cat.title ? <UnderLine variants={UnderLineVariants} initial="hidden" animate="visible" /> : null}</Category>))}
-                {IsGranted && <Category onClick={()=>setIsSelected('게시판 관리')} to={`/study/${studyId}/board/manage`}>
-                    게시판 관리{IsSelected === '게시판 관리' ? <UnderLine variants={UnderLineVariants} initial="hidden" animate="visible" /> : null }
-                    </Category>}
-            </CategoryWrapper>
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <CategoryWrapper>
+        {category?.map((cat) => (
+          <Category
+            activeclassname="active"
+            onClick={() => setIsSelected(cat.title)}
+            to={`/study/${studyId}/board/${cat.studyBoardId}/articles`}
+            key={cat.studyBoardId}
+          >
+            {cat.title}
+            {IsSelected === cat.title ? (
+              <UnderLine
+                variants={UnderLineVariants}
+                initial="hidden"
+                animate="visible"
+              />
+            ) : null}
+          </Category>
+        ))}
+        {IsGranted && (
+          <Category
+            onClick={() => setIsSelected("게시판 관리")}
+            to={`/study/${studyId}/board/manage`}
+          >
+            게시판 관리
+            {IsSelected === "게시판 관리" ? (
+              <UnderLine
+                variants={UnderLineVariants}
+                initial="hidden"
+                animate="visible"
+              />
+            ) : null}
+          </Category>
+        )}
+      </CategoryWrapper>
+      <Outlet />
+    </>
+  );
 }
 
 export default StudyBoard;
